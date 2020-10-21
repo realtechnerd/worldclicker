9bb4be4ea3a7776322a85a8bb331ad4b55f2bc08
realtechnerd <40891074+realtechnerd@users.noreply.github.com>
2020-10-21 12:01:06 -0400

    Updates

---
+++ precache-manifest.382e6bbe3fe49795957dd2d63b97a916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702eb191e2ad70a7668c22b550808374",
+    "url": "/worldclicker/index.html"
+  },
+  {
+    "revision": "730d212552d3e1c5f67c",
+    "url": "/worldclicker/static/css/main.c9951fe3.chunk.css"
+  },
+  {
+    "revision": "343ed3f897258cf07982",
+    "url": "/worldclicker/static/js/2.1d232812.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/worldclicker/static/js/2.1d232812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730d212552d3e1c5f67c",
+    "url": "/worldclicker/static/js/main.82ce8d75.chunk.js"
+  },
+  {
+    "revision": "56363c202c9c0b6b096a",
+    "url": "/worldclicker/static/js/runtime-main.3629c4df.js"
+  }
+]);
